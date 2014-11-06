04ce738dce4049b9085e2cd05ab5ccb1cca6e6d3
Casper Steinmann <casper.steinmann@gmail.com>
2014-11-06 21:35:33 +0100

    Updates SDLWindow to use proper SDL2 API

--- engine/base/sdlwindow.cc
+++ engine/base/sdlwindow.cc
@@ -23,14 +23,15 @@ void SDLWindow::Start(int width, int height) {
 
     SDL_Init( SDL_INIT_VIDEO );
     window = SDL_CreateWindow( "GAME", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
-    if ( window != NULL ) {
-        screen = SDL_GetWindowSurface( window );
-        if ( screen == NULL ) {
-            CLOG(ERROR, "SDLWindow") << "Could not create SDL_Surface.";
+    if ( window != nullptr ) {
+        renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
+        if ( renderer == nullptr ) {
+            CLOG(ERROR, "SDLWindow") << "Could not create SDL_Renderer." << SDL_GetError();
             Stop();
         }
+        SDL_RenderSetLogicalSize( renderer, width, height );
     } else {
-        CLOG(ERROR, "SDLWindow") << "Could not create SDL_Window.";
+        CLOG(ERROR, "SDLWindow") << "Could not create SDL_Window: " << SDL_GetError();
     }
 }
 
@@ -40,8 +41,11 @@ void SDLWindow::Stop() {
 }
 
 void SDLWindow::Swap() {
-    if ( screen != NULL ) {
-        SDL_UpdateWindowSurface( window );
+    if ( renderer != NULL ) {
+        SDL_SetRenderDrawColor( renderer, 128, 0, 0, 255 );
+        SDL_RenderClear( renderer );
+
+        SDL_RenderPresent( renderer );
     }
 }
 
