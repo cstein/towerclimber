11e059b7684e5daa286adb2f7d2ba16d0f84350c
Casper Steinmann <casper.steinmann@gmail.com>
2014-11-09 16:26:54 +0100

    Obtain quad coordinates for string.

--- game/main.cc
+++ game/main.cc
@@ -10,6 +10,36 @@
 
 _INITIALIZE_EASYLOGGINGPP
 
+struct FontVert {
+    float _x;
+    float _y;
+    FontVert():
+        _x(0.0f),
+        _y(0.0f) {
+    }
+    FontVert(float x, float y) {
+        _x = x;
+        _y = y;
+    }
+    void print() {
+        LOG(INFO) << "VERTEX: x, y = " << _x << " " << _y;
+    }
+};
+
+struct FontQuad {
+    FontVert tl;
+    FontVert bl;
+    FontVert br;
+    FontVert tr;
+
+    void print() {
+        tl.print();
+        bl.print();
+        br.print();
+        tr.print();
+    }
+};
+
 int main() {
     bool running = true;
 
@@ -30,6 +60,32 @@ int main() {
 
     FontManager* fontmanager = new FontManager();
     fontmanager->Start();
+
+    // attempt to build quads
+    std::string quad = "Casper!";
+    Font* f = fontmanager->Get("Ubuntu Mono");
+    LOG(INFO) << "loaded font size: " << f->GetSize();
+
+    float x, y;
+    x = 0.0f;
+    y = 0.0f;
+    for(std::string::iterator it = quad.begin(); it < quad.end(); it++) {
+        std::stringstream ss;
+        std::string c;
+        ss << *it;
+        ss >> c;
+        CharacterRect* r = f->GetCharRect( c );
+        // lets build a quad
+        LOG(INFO) << "character '" << *it << "' rect: x=" << r->x << " y=" << r->y << " w=" << r->w << " h=" << r->h << " Qx=" << x;
+        FontQuad q = FontQuad();
+        q.tl = FontVert( x, y );
+        q.bl = FontVert( x, y + (float)r->h );
+        q.br = FontVert( x + (float)r->w, y + (float)r->h );
+        q.tr = FontVert( x + (float)r->w, y );
+        q.print();
+        x = x + (float)r->w;
+    }
+
     while(running) {
         controls->Update( scenemanager->GetActive() );
         running = !controls->IsExitState();
